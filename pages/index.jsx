163ecd8c82331143bdbc52c432cf1a73b4dbdd1f
chenyanfei <15873624283@163.com>
2022-10-31 15:55:18 +0800

    feat: tailwind+scss

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>ChenYanfei</title>
+        <meta name="description" content="movie,music,image" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className={styles.hero + ' text-5xl text-center p-8'}>Welcome to my website</h1>
+    </div>
+  )
+}
